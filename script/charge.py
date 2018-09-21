a5c5a6eb7294105e3c594678b171fd3140eeada1
jianhuizhai <jianhuizhai89@gmail.com>
2018-09-21 15:24:22 +0200

    Add files via upload

--- script/charge.py
+++ script/charge.py
@@ -1,27 +1,27 @@
 import numpy as np
 import sys
 
-OldNewFileName = sys.argv[1]
+OldFileName = sys.argv[1]
 skipline = int(sys.argv[2])
 NewFileName = sys.argv[3]      # NewFileName is the name of new file
 
-print('old file name is', OldNewFileName)
+print('old file name is', OldFileName)
 print('new file name is', NewFileName)
 
-data = np.loadtxt(OldNewFileName, skiprows=(skipline))
+data = np.loadtxt(OldFileName, skiprows=(skipline))
 newfile = open(NewFileName, 'w')
 
-f = open(OldNewFileName,'r')
+oldfile = open(OldFileName,'r')
 for i in range(skipline):
-    line = f.readline()
+    line = oldfile.readline()
     newfile.write(line)
-f.close()
+oldfile.close()
 
 for i in range(len(data)):
     if(int(data[i][1])==1):
         charge = 2
     else:
         charge = -2
-    line = '%10i %5i %8.2f %20.8f %20.8f %20.8f\n'%(i+1, data[i][1], charge, data[i][2], data[i][3], data[i][4])
+    line = '%10i %5i %8.2f %16.8f %16.8f %16.8f\n'%(i+1, data[i][1], charge, data[i][2], data[i][3], data[i][4])
     newfile.write(line)
 newfile.close()
