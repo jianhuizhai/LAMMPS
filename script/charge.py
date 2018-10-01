31fe3bbffc346ddc47ab006e36e43283ada71dd2
jianhuizhai <jianhuizhai89@gmail.com>
2018-10-01 21:44:47 +0200

    Add files via upload

--- script/charge.py
+++ script/charge.py
@@ -1,13 +1,18 @@
 import numpy as np
 import sys
 
+
 OldFileName = sys.argv[1]
-skipline = int(sys.argv[2])
-NewFileName = sys.argv[3]      # NewFileName is the name of new file
+# skipline = int(sys.argv[2])
+NewFileName = sys.argv[2]      # NewFileName is the name of new file
 
 print('old file name is', OldFileName)
 print('new file name is', NewFileName)
 
+# for skipline you can use if command for different crystal lattices.
+# here 16 is used for a lattice type contains two atom types.
+skipline = 16    
+   
 data = np.loadtxt(OldFileName, skiprows=(skipline))
 newfile = open(NewFileName, 'w')
 
@@ -17,11 +22,16 @@ for i in range(skipline):
     newfile.write(line)
 oldfile.close()
 
+totalcharge = 0.0
 for i in range(len(data)):
     if(int(data[i][1])==1):
-        charge = 2
+        charge = 1.7
     else:
-        charge = -2
-    line = '%10i %5i %8.2f %16.8f %16.8f %16.8f\n'%(i+1, data[i][1], charge, data[i][2], data[i][3], data[i][4])
+        charge = -1.7
+    totalcharge = totalcharge + charge
+    line = '%10i %5i %8.1f %16.8f %16.8f %16.8f\n'%(i+1, data[i][1], charge, data[i][2], data[i][3], data[i][4])
     newfile.write(line)
 newfile.close()
+
+print('total charge is', totalcharge)
+print('The file is successfully changed!!')
