93b67cc6a2a5f54eaa0e2b38ac8f402e72d13870
jianhuizhai <jianhuizhai89@gmail.com>
2018-10-09 12:32:33 +0200

    Add files via upload

--- script/charge.py
+++ script/charge.py
@@ -11,20 +11,33 @@ print('new file name is', NewFileName)
 
 # for skipline you can use if command for different crystal lattices.
 # here 16 is used for a lattice type contains two atom types.
+
+skipmass = int(input("the skipline for mass (the line do not contains Masses keyword): "))
 skipline = 16
 
 data = np.loadtxt(OldFileName, skiprows=(skipline))
 newfile = open(NewFileName, 'w')
 
 oldfile = open(OldFileName,'r')
-for i in range(skipline):
-    if (i == skipline-2):
-        line = "Atoms # charge\n"
-    elif(i == skipline-1):
-        line = "\n"
-    else:
-        line = oldfile.readline()
+
+for i in range(skipmass):
+    line = oldfile.readline()
     newfile.write(line)
+
+line = 'Masses'+'\n'
+newfile.write(line)
+line = '\n'
+newfile.write(line)
+line = '           1   24.30500000    # Mg'+'\n'
+newfile.write(line)
+line = '           2   15.99900000    # O'+'\n'
+newfile.write(line)
+line = '\n'
+newfile.write(line)
+line = "Atoms # charge\n"
+newfile.write(line)
+line = '\n'
+newfile.write(line)
 oldfile.close()
 
 totalcharge = 0.0
@@ -34,7 +47,7 @@ for i in range(len(data)):
     else:
         charge = -1.7
     totalcharge = totalcharge + charge
-    line = '%10i %5i %8.1f %16.8f %16.8f %16.8f\n'%(i+1, data[i][1], charge, data[i][2], data[i][3], data[i][4])
+    line = '%10i %5i %8.1f %16.8f %16.8f %16.8f\n'%(data[i][0], data[i][1], charge, data[i][2], data[i][3], data[i][4])
     newfile.write(line)
 newfile.close()
 
