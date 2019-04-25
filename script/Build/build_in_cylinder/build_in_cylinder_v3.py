ce683309a174f2b3ec4ba1f9ff211ec6aa96f6c6
jianhui <jianhuizhai89@gmail.com>
2019-04-25 19:46:47 +0200

    Update build_in_cylinder_v3.py

--- script/Build/build_in_cylinder/build_in_cylinder_v3.py
+++ script/Build/build_in_cylinder/build_in_cylinder_v3.py
@@ -6,7 +6,6 @@ from math import ceil
 import matplotlib.pylab as plt
 from mpl_toolkits import mplot3d
 
-
 #==================================================================================
 #                 define a function to create build.sh in specified folder
 #==================================================================================
@@ -28,25 +27,7 @@ def mk_build( filename, length, atom_delete, atom_id ):
     f.write(line)
     '''
 
-    #len_deleted = len(deleted_atom)
     str_line = 'atomsk ../relax.lmp '
-    """
-    if( len_deleted <= 17):
-        #line1    = '-select ' + ','.join(str(int(i)) for i in deleted_atom ) + ' -rmatom select '
-        line1    = '-select ' + ','.join(str(int(i)) for i in deleted_atom )
-        str_line = str_line + line1
-    else:
-        # a mention to change the input file (noclimb.lmp file)
-        exit("The number of deleted atoms are already 18.") 
-        '''
-        repeat = int( ceil(len_deleted/18) )
-        
-        for i in range(repeat):
-            #line1 = '-select ' + ','.join(str(int(k)) for k in deleted_atom[10*i:10*i+10] ) + ' -rmatom select '
-            line1 = '-select ' + ','.join(str(int(k)) for k in deleted_atom[10*i:10*i+10] )
-            str_line = str_line + line1
-        '''   
-    """
     if( length%2 == 0 ):
         line = str_line + '-select %i -rmatom select -add-atom %s at 224.855 150.6465 82.2513 initial.lmp \n' %( int(atom_id), atom_delete )
         print(line)
@@ -63,10 +44,6 @@ def mk_build( filename, length, atom_delete, atom_id ):
 
     f.close()
 
-    #s = open(filename).read()
-    #s = s.replace('*****',atom_id)
-    #f = open(filename, 'w')
-    #f.close()
 #==================================================================================
 linecommon = "=========================================================================="
 
@@ -78,12 +55,12 @@ x_d, y_d, z_d = np.loadtxt('deleted.dat', usecols=(1,2,3), unpack=True)
 #                   cd the folder which has the lowest energy
 #==================================================================================
 parent_folder = np.loadtxt('energy_info.dat',dtype=int, usecols=(0))[0]
-#print(parent_folder)
 #print(str(parent_folder))
 os.chdir( str(parent_folder) )
 print(os.getcwd())
 
 #==================================================================================
+print(linecommon)
 atom_delete = input("The deleted ion type : " ).lower()
 if(atom_delete == 'mg'):
     delete_type =1
@@ -98,14 +75,11 @@ radius = float(input("The radius of cylinder (3 or 2.5) : "))
 ions=[]
 
 print(linecommon)
-#dumpfile=""
 bash_return, dumpfile = subprocess.getstatusoutput('(ls -t dump.relax* | head -n 1)')
-#os.system('dumpfile=$(ls -t dump.relax* | head -n 1)')
-#os.system('export dumpfile')
+
 print("lmp_file : ", dumpfile )
-#print("lmp_file : ", os.getenv("dumpfile") )
-#print("dat_file : deleted.dat")
 print("loading files ...")
+
 #=================================================================================
 #                   create corresponed vacancy folder
 #=================================================================================
@@ -113,6 +87,8 @@ v_folder = 'v_' + atom_delete
 if not os.path.exists( v_folder ):
     os.mkdir( v_folder )
 os.chdir( v_folder )
+
+print(linecommon)
 print( os.getcwd() )
 
 #=================================================================================
@@ -129,16 +105,12 @@ f.write(line)
 f.close()
 os.system('bash build_relax.sh')
 
-
 #=================================================================================
-
-#atomid, atom_type, x, y, z  = np.loadtxt(, skiprows=16, usecols=(0,1,2,3,4), unpack=True)
 print(linecommon)
 print("load relax.lmp")
 
 atomid, atom_type, x, y, z  = np.loadtxt('relax.lmp', skiprows=16, usecols=(0,1,2,3,4), unpack=True)
-#deleted_atom, x_d, y_d, z_d = np.loadtxt('deleted.dat', usecols=(0,2,3,4), unpack=True)
-atomid = np.array(atomid, dtype = int)
+atomid = np.array(atomid, dtype = int)  # change to int type to create folder
 
 xc = np.mean(x_d)
 yc = np.mean(y_d)
@@ -161,8 +133,6 @@ test_info.write(line)
 
 for i in range(len(atomid)):
     if(atom_type[i] == delete_type):
-        #if ( all( [atomid[i]  ) ):
-        #if(abs(z[i] - zc) <= 5.0):
         r2 = (x[i] - xc)**2 + (y[i] - yc)**2
         if(r2 <= radius**2 and z[i] <= z_max + 8.5 and z[i] >= z_min - 8.5 ):
             ions.append( atomid[i] )
@@ -177,25 +147,12 @@ print("Write distribution.dat")
 print("The selected number of ions are : ", len(ions))
 
 #====================================================================================================
-
-'''
-print('type(ions) is ', type(ions))
-ions = np.array( ions, dtype = int )  # change list to int array. 
-print('type(ions) is ', type(ions))
-
-for atom in ions:
-    folder = str(atom)
-    print(folder)
-'''
-
-#====================================================================================================
 #               plot the chosen ions type
 #====================================================================================================
 print(linecommon)
 print("Plot point distribution")
 
 data = np.loadtxt('distribution.dat')
-#print(data)
 ax = plt.axes(projection='3d')
 #ax.scatter(x, y, z, c='r')  # 绘制数据点,颜色是红色
 #plt.scatter(data[:,2],data[:,3], data[:,4], cmap='coolwarm')
@@ -207,26 +164,20 @@ plt.ylim(yc - 4*radius, yc+4*radius)
 plt.savefig('point_distribution.pdf')
 plt.show()
 
-
-
 #====================================================================================================
 filename = 'build_noclimb.sh'
-print("=============================================================")
+print(linecommon)
 
 os.system('ls')
 flag_floder     = input("Do you want to clean earlier results          (y or n) \n \
 This will delete all the folders in current folder.   : ")
-##flag_interstitial = input("Do you want to add %s interstitial (y or n) : " %(atom_delete)).lower()
-#os.system('rm -f energy_info.dat')
+
 if(flag_floder == 'y'):
     for folder in os.listdir():
         if(os.path.isdir(folder)):
             if(folder != 'reference' and folder != '__pycache__' and folder != 'v_mg' and folder != 'v_o'):
                 os.system('rm -r '+folder)
-            #os.rmdir(folder)
-            #os.chdir(folder)
-            #os.system('clean.sh')
-            #os.chdir("../")
+
 for atom in ions:
     print('atom id is : ', atom)
     folder = str(atom)
