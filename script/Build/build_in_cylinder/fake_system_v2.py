4a787e941c1c38602dd7fded1eb4a8efbb585318
jianhuizhai <jianhuizhai89@gmail.com>
2019-05-07 15:00:35 +0200

    add the condition when the length of deleted.dat is zero

--- script/Build/build_in_cylinder/fake_system_v2.py
+++ script/Build/build_in_cylinder/fake_system_v2.py
@@ -13,8 +13,6 @@ from output_fake import DumpOutput
 #=================================================================================
 linecommon = '========================================================='
 
-print(linecommon)
-
 lmp_file = sys.argv[1]
 
 #=================================================================================
@@ -26,17 +24,14 @@ print("deleted_file: deleted.dat")
 print("lmp_file    : ", lmp_file)
 print("energy_file : energy_info.dat")
 print("loading files...")
-"""
+
 xlim = linecache.getline(lmp_file, 6)
 ylim = linecache.getline(lmp_file, 7)
 zlim = linecache.getline(lmp_file, 8)
-
 print(linecommon)
 print(xlim, end='')
 print(ylim, end='')
 print(zlim, end='')
-print(linecommon)
-"""
 
 #=================================================================================
 #                       loading files
@@ -44,16 +39,12 @@ print(linecommon)
 skipline  = 16
 atom_id, atom_type, x,y,z = np.loadtxt(lmp_file, skiprows=skipline, usecols=(0,1,2,3,4), unpack=True)
 eng_id, eng_eng = np.loadtxt('energy_info.dat', usecols=(0,6), unpack=True)
-id_d, x_d, y_d, z_d = np.loadtxt("deleted.dat", usecols=(0,1,2,3))
-print(linecommon)
-print("The number of already deleted ions are %i: "  %x_d.size)
 
 #=================================================================================
 #               change array to list (eassy to append data)
 #=================================================================================
 #          add already deleted data to atom_type, x, y and z; and assign Eng
 #=================================================================================
-
 atom_id   = list(atom_id)
 atom_type = list(atom_type)
 x         = list(x)
@@ -61,22 +52,26 @@ y         = list(y)
 z         = list(z)
 Eng       = [20]*len(atom_id)
 
-len_new = len(atom_id) + x_d.size
-if( x_d.size == 1):
-    
-    atom_type.append(id_d)
-    x.append( x_d )         
-    y.append( y_d )
-    z.append( z_d )
-    Eng.append( 5 )
-else:
-    for i in range(x_d.size):
-        atom_type.append(id_d[i])
-        x.append( x_d[i] )
-        y.append( y_d[i] )
-        z.append( z_d[i] )
-        Eng.append( 5 )
+#length = len( open('deleted.dat').read() )
+length = len( open(r"deleted.dat",'rU').readlines())
+if length != 0:
+    id_d, x_d, y_d, z_d = np.loadtxt("deleted.dat", usecols=(0,1,2,3), unpack=True)
 
+    len_new = len(atom_id) + x_d.size
+    if( x_d.size == 1 ):
+    
+        atom_type.append(id_d)
+        x.append( x_d )         
+        y.append( y_d )
+        z.append( z_d )
+        Eng.append( 5 )
+    else:
+        for i in range(x_d.size):
+            atom_type.append(id_d[i])
+            x.append( x_d[i] )
+            y.append( y_d[i] )
+            z.append( z_d[i] )
+            Eng.append( 5 )
 #--------------------------------------------------------------------------------
 #                assign the energy values of distributed ions to Eng
 #--------------------------------------------------------------------------------
@@ -84,6 +79,10 @@ for k in range(eng_id.size):
     id = atom_id.index( eng_id[k] )
     Eng[id] = eng_eng[k]
 
+print(linecommon)
+print("The number of already deleted ions are %i: "  %length )
+
+
 #=================================================================================
-DumpOutput('dump.vacancies',xlim, ylim, zlim, atom_id, atom_type, x, y, z, Eng)
-print("Output file: dump.vacancies")
+DumpOutput('dump.vacancies'+str(length),xlim, ylim, zlim, atom_id, atom_type, x, y, z, Eng)
+print("Output file: dump.vacancies"+str(length) )
