0f338dcae004a0d3d0ad5e30cb6307bd848c05ef
jianhuizhai <jianhuizhai89@gmail.com>
2019-05-21 12:00:02 +0200

    Update job_scancle.py

--- job/job_scancle.py
+++ job/job_scancle.py
@@ -1,7 +1,7 @@
 import os
 
-xlo = int(input("minimun running id : "))
-xhi = int(input("maximum running id : "))
+xlo = int( input("minimun running id : ") )
+xhi = int( input("maximum running id : ") )
 
 for i in range(xlo, xhi+1, 1):
     os.system('scancel '+str(i))
