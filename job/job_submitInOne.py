b0282cc43550d344ab52d124adf28aaf5b101aee
jianhuizhai <jianhuizhai89@gmail.com>
2019-05-23 13:49:54 +0200

    add command to record how many are runned.

--- job/job_submitInOne.py
+++ job/job_submitInOne.py
@@ -3,17 +3,22 @@ import numpy as np
 #import commands  # commands for python2.x
 import subprocess # subprocess is same as commands, but it for python3.x
 
-#count = 0
+count = 0
 if not os.path.exists('runned.dat') :
     exit("runned.dat does not exixt.")
+runned = np.loadtxt('runned.dat',usecols=([0]))
+'''
+for k in runned:
+    print('k = ', str(int(k)) )
+'''
 print('='*80)
 print("This will not submit job in reference folder.")
 for folder in os.listdir('.'):
     if(os.path.isdir(folder) and folder != 'reference'):
-            if not any( [folder == str( int(k) ) for k in np.loadtxt('runned.dat',usecols=([0])) ] ):
+            if not any( [folder == str( int(k) ) for k in runned ] ):
                 print('='*80)
                 print(folder)
-#                count += 1
+                count += 1
                 '''
                 running lammps
                 '''
@@ -22,10 +27,11 @@ for folder in os.listdir('.'):
                 os.system('rm -f *.dat *.slurm')
                 bash_return, inputfile = subprocess.getstatusoutput('(ls -t in* | head -n 1)')
                 #bash_return, dumpfile = commands.getstatusoutput('(ls -t in* | head -n 1)')
-                #os.system('mpirun -np 1 lmp_mpi -in '+inputfile) '''for test'''
+                os.system('mpirun -np 1 lmp_mpi -in '+inputfile) ###for test
 #                os.system('mpiexec lmp < '+inputfile+' > lammps.out')
                 os.chdir( '../' )
                 with open('runned.dat', 'a') as f:
                     f.write(folder+'\n')
-
+with open('runned.dat', 'a') as f:
+    f.write('# 　'+str(count+len(runned)-1)+'\n')
 #print( 'count = ', count)
